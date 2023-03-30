b77dc2c63f9015394770a904ec07415526ef884b
Fotios N. Zachopoulos <zaxfotis@gmail.com>
2023-03-30 13:33:30 +0300

    fix css in CardExploreDark

--- components/common/CardExploreDark.tsx
+++ components/common/CardExploreDark.tsx
@@ -17,11 +17,11 @@ function CardExploreDark({ image, text }: CardExploreDarkProps) {
   return (
     <article
       className="relative flex-1 w-full rounded-md shadow-lg overflow-hidden
-      min-w-[400px] h-[350px] mobile:min-w-full
+      min-w-[400px] min-h-[350px] mobile:min-w-full
       hover:shadow-2xl hover:scale-105 transition-all duration-300"
     >
-      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-l from-transparent to-[#000] opacity-90" />
-      <div className="absolute w-full h-full top-0 left-0 p-10 text-white flex flex-col justify-center items-start gap-5">
+      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-l from-transparent to-[#000] opacity-90 -z-10" />
+      <div className="w-full h-full p-10 text-white flex flex-col justify-center items-start gap-5">
         <h3 className="text-h3 font-[500] leading-tight w-full max-w-[600px]">
           <Balancer className="w-full">{text.title}</Balancer>
         </h3>
@@ -32,7 +32,7 @@ function CardExploreDark({ image, text }: CardExploreDarkProps) {
           <Button text={text.button} url={text.url} variant={'underlined'} size="md" />
         </div>
       </div>
-      <Image src={image} alt="Stat Image" className="h-full w-full object-cover" />
+      <Image src={image} alt="Stat Image" className="absolute top-0 left-0 h-full w-full object-cover -z-20" />
     </article>
   );
 }
--- components/common/Section.tsx
+++ components/common/Section.tsx
@@ -18,16 +18,16 @@ function Section({ title, text = '', children }: SectionProps) {
       transition={{ duration: 0.5 }}
     >
       <div className="max-w-[1280px] mx-auto flex flex-col justify-start items-start gap-[10px]">
-        <h1 className="w-full relative text-black text-h1 font-[500]">
+        <h1 className="relative text-black text-h1 font-[500] px-5">
           <Balancer>{title}</Balancer>
           {/* Left Green Line */}
           <span
-            className="absolute -top-[5px] -left-[20px] mobile:hidden
+            className="absolute -top-[5px] left-0
             w-[6px] h-[40px] bg-secondary rounded-b-sm"
           />
           {/* Top Green Line */}
           <span
-            className="absolute -top-[5px] -left-[20px] mobile:left-0 mobile:w-[60px]
+            className="absolute -top-[5px] left-0 mobile:w-[60px]
             w-[40px] h-[6px] bg-secondary rounded-r-sm"
           />
         </h1>
